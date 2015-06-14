e4bba745e1cf28ef5c971202b71b0eab738bab40
Reza Zardosht <rzhreza@gmail.com>
2015-06-14 15:08:49 +0430

    OkConnectWithChatServer
    the last program that full communicate with  delphi chat server

--- src/com/lakj/comspace/simpletextclient/DataRecieveHandle.java
+++ src/com/lakj/comspace/simpletextclient/DataRecieveHandle.java
@@ -3,29 +3,184 @@ package com.lakj.comspace.simpletextclient;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//#define  MAX_PACK_LENGTH  60
 
 /**
  * Created by Zardosht on 06/09/2015.
  */
+
+
 public class DataRecieveHandle extends BroadcastReceiver {
 
-    static int mycall=0;
-    SlimpleTextClientActivity main = null;
-    void setMainActivityHandler(SlimpleTextClientActivity main){
-        this.main=main;
+
+    String strJson = "{\"Status\": {" +
+            "\"CHWOutLetTemp\": \"0.0\"," +
+            "\"HTWOutLetTemp\": \"0.0\"," +
+            "\"RefTemp\": \"0.0\"," +
+            "\"ValvePosition\": \"0.0\"," +
+            "\"CHWInLetTemp\": \"0.0\"," +
+            "\"HTWInLetTemp\": \"0.0\"," +
+            "\"COWOutLetTemp\": \"0.0\"," +
+            "\"CowInLetTemp\": \"0.0\"," +
+            "\"DilutionTemp\": \"0.0\"," +
+            "\"SolutionTemp\": \"0.0\"," +
+            "\"ExhaustTemp\": \"0.0\"" +
+            "  }," +
+            "\"Setting\":{" +
+            "\"CHWOutLetTemp\": \"0.0\"," +
+            "\"HTWOutLetTemp\": \"0.0\"," +
+            "\"RefTemp\": \"0.0\"," +
+         /*   "\"ValvePosition\": \"0.0\"," +
+            "\"CHWInLetTemp\": \"0.0\"," +
+            "\"HTWInLetTemp\": \"0.0\"," +
+            "\"COWOutLetTemp\": \"0.0\"," +
+            "\"CowInLetTemp\": \"0.0\"," +
+            "\"DilutionTemp\": \"0.0\"," +
+            "\"SolutionTemp\": \"0.0\"," +
+          */  "\"ExhaustTemp\": \"0.0\"" +
+            "}}";
+    JSONObject jsonRootObject;
+
+    public DataRecieveHandle() {
+        try {
+            jsonRootObject = new JSONObject(strJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
+
+    public enum FollowLogic {
+        RequestHeader,
+        RequestBody,
+        ResponseHeader,
+        ResponseBody,
+    }
+
+    char[] RequestHeader = new char[10];
+    char[] RequestBody = new char[100];
+    char[] ResponseHeader = new char[10];
+    char[] ResponseBody = new char[100];
+    int RequestBodyLength = 0;
+    int ResponseBodyLength = 0;
+    int[] RecivedData = new int[100];
+    int ConstReqPos = 0;
+    SlimpleTextClientActivity main = null;
+    FollowLogic PointFollowLogic = FollowLogic.RequestHeader;
+    int MaxValueForCurrentFolloLogic = 7;
+
+    void setMainActivityHandler(SlimpleTextClientActivity main) {
+        this.main = main;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        mycall++;
-        String MyStr;
-        MyStr ="";
+        int i, j;
+        String MyStr, MyStr2;
+        MyStr2 = MyStr = "";
         byte[] bytes;
         bytes = intent.getByteArrayExtra("Data");
         int bytesLength = intent.getIntExtra("Data_Length", 0);
-        for(int i=0;i<bytesLength;i++)
-            MyStr = MyStr+"," +(char) bytes[i];
-     //   Toast.makeText(context, "I Data Reciever Intent Detected.@"+mycall+"@+"+ MyStr, Toast.LENGTH_LONG).show();
-           main.setViewNoStatic(MyStr);
+        for (i = 0; i < bytesLength; i++) {
+            RecivedData[ConstReqPos++] = bytes[i];
+            if (ConstReqPos < 7) continue;
+            if (ConstReqPos > 60) {
+                ConstReqPos = 0;
+                continue;
+            }
+            //Todo check for all Request Type
+            if (RecivedData[ConstReqPos - 7] == 0x02 && RecivedData[ConstReqPos - 6] == 0x7D && RecivedData[ConstReqPos - 5] == 0x65 &&
+                    RecivedData[ConstReqPos - 4] == -1 && RecivedData[ConstReqPos - 3] == -1 &&
+                    (RecivedData[ConstReqPos - 2] == -111 || RecivedData[ConstReqPos - 2] == -112) && RecivedData[ConstReqPos - 1] == 0x77) {
+                for (j = 0; j < 7; j++)
+                    RequestHeader[j] = (char) RecivedData[ConstReqPos - (7 - j)];
+                for (j = 0; j < ConstReqPos - 7; j++)
+                    ResponseBody[j] = (char) RecivedData[j];
+                ResponseBodyLength = ConstReqPos - 7;
+                MyStr = "1 -->";
+                for (j = 0; j < ConstReqPos - 7; j++)
+                    MyStr = MyStr + "," + String.format("%02X", (ResponseBody[j] & 0xFF));
+                MyStr = MyStr + " ,Shck = " + CheckPackageSum(ResponseHeader, ResponseBody, ConstReqPos - 7);
+                CheckResponsePackage();
+//               main.setViewNoStatic(MyStr + MyStr2);
+                Log.d("DataEncript ", MyStr);
+                ConstReqPos = 0;
+                //    RecivedData[ConstReqPos++] = bytes[i];
+
+            } else if (RecivedData[ConstReqPos - 7] == 0x02 && RecivedData[ConstReqPos - 6] == -1 && RecivedData[ConstReqPos - 5] == -1 &&
+                    RecivedData[ConstReqPos - 4] == 0x7D && RecivedData[ConstReqPos - 3] == 0x65 && RecivedData[ConstReqPos - 2] == 0x06 &&
+                    (RecivedData[ConstReqPos - 1] == -111 || RecivedData[ConstReqPos - 1] == -112)) {
+                for (j = 0; j < 7; j++)
+                   ResponseHeader[j] = (char) RecivedData[ConstReqPos - (7 - j)];
+                for (j = 0; j < ConstReqPos - 7; j++)
+                    RequestBody[j] = (char) RecivedData[j];
+                RequestBodyLength = ConstReqPos - 7;
+                MyStr2 = "2 -->";
+                for (j = 0; j < ConstReqPos - 7; j++)
+                    MyStr2 = MyStr2 + "," + String.format("%02X",( RequestBody[j] & 0xFF));
+                MyStr2 = MyStr2 + " ,Shck= " + CheckPackageSum(RequestHeader, RequestBody, ConstReqPos - 7);
+                //              main.setViewNoStatic(MyStr + MyStr2);
+                Log.d("DataEncript ", MyStr2);
+                ConstReqPos = 0;
+                //  RecivedData[ConstReqPos++] = bytes[i];
+
+            }
+
+        }
     }
 
+    public void CheckRequestPackage() {
+        if (!CheckPackageSum(RequestHeader, RequestBody, RequestBodyLength)) return;
+
+        if (RequestHeader[5] == -112) {//0x90
+            ////////
+        }
+    }
+
+    public void CheckResponsePackage() {
+        //Todo check check sum
+        if(ResponseBodyLength<7 )return;
+        if (!CheckPackageSum(ResponseHeader, ResponseBody, ResponseBodyLength)) return;
+       try {
+           String MStr="";
+           MStr = MStr + ResponseBody[0];
+           MStr = MStr + ResponseBody[1];
+           MStr = MStr + ResponseBody[3];
+           Log.d("DataEncript ",MStr);
+           if (ResponseBody[0] == 2 && ResponseBody[1] == 5 && ResponseBody[3] == 0) {
+               float value = (ResponseBody[8] & 0Xff) * 256 + (ResponseBody[9] & 0XFF);
+               Log.d("DataEncript ",  String.format("%2.2f", value / 100));
+               switch (ResponseBody[2]) {
+                   case 0:
+                       jsonRootObject.getJSONObject("Status").put("CHWOutLetTemp", String.format("%2.2f", value / 256));
+                       break;
+                   case 1:
+                       jsonRootObject.getJSONObject("Status").put("HTWOutLetTemp", String.format("%2.2f", value / 256));
+                       break;
+                   case 2:
+                       jsonRootObject.getJSONObject("Status").put("RefTemp", String.format("%2.2f", value / 256));
+                       break;
+                   case 3:
+                       jsonRootObject.getJSONObject("Status").put("ValvePosition", String.format("%2.2f", value));
+                       break;
+               }
+           }
+       } catch (JSONException e) {
+           e.printStackTrace();
+       }
+        main.setViewNoStatic(jsonRootObject);
+    }
+
+    boolean CheckPackageSum(char[] header, char[] body, int Length) {
+        if(Length<7) return false;
+        int Sum = 0;
+        for (int j = 0; j < 7; j++)
+            Sum = (header[j] & 0xFF) + Sum;
+        for (int j = 0; j < Length - 1; j++)
+            Sum = Sum + (body[j] & 0XFF);
+        return (Sum & 0XFF) == (body[Length - 1] & 0XFF);
+    }
 }
--- src/com/lakj/comspace/simpletextclient/SlimpleTextClientActivity.java
+++ src/com/lakj/comspace/simpletextclient/SlimpleTextClientActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -23,14 +24,38 @@ import java.net.UnknownHostException;
 
 /**
  * This is a simple Android mobile client
- * This application read any string massage typed on the text field and 
+ * This application read any string massage typed on the text field and
  * send it to the server when the Send button is pressed
  * Author by Lak J Comspace
- *
  */
 public class SlimpleTextClientActivity extends Activity {
-
-    static int intentCallCount=0;
+    String strJson = "{\"Status\": {" +
+            "\"CHWOutLetTemp\": \"0.0\"," +
+            "\"HTWOutLetTemp\": \"0.0\"," +
+            "\"RefTemp\": \"0.0\"," +
+            "\"ValvePosition\": \"0.0\"," +
+            "\"CHWInLetTemp\": \"0.0\"," +
+            "\"HTWInLetTemp\": \"0.0\"," +
+            "\"COWOutLetTemp\": \"0.0\"," +
+            "\"CowInLetTemp\": \"0.0\"," +
+            "\"DilutionTemp\": \"0.0\"," +
+            "\"SolutionTemp\": \"0.0\"," +
+            "\"ExhaustTemp\": \"0.0\"" +
+            "  }," +
+            "\"Setting\":{" +
+            "\"CHWOutLetTemp\": \"0.0\"," +
+            "\"HTWOutLetTemp\": \"0.0\"," +
+            "\"RefTemp\": \"0.0\"," +
+            "\"ValvePosition\": \"0.0\"," +
+            "\"CHWInLetTemp\": \"0.0\"," +
+            "\"HTWInLetTemp\": \"0.0\"," +
+            "\"COWOutLetTemp\": \"0.0\"," +
+            "\"CowInLetTemp\": \"0.0\"," +
+            "\"DilutionTemp\": \"0.0\"," +
+            "\"SolutionTemp\": \"0.0\"," +
+            "\"ExhaustTemp\": \"0.0\"" +
+            "}}";
+    static int intentCallCount = 0;
     // default ip
     public static String SERVERIP = "192.168.1.6";
 
@@ -38,40 +63,27 @@ public class SlimpleTextClientActivity extends Activity {
     public static final int SERVERPORT = 502;
 
     private Socket client;
-	private PrintWriter printwriter;
-	private EditText textField;
+    private PrintWriter printwriter;
+    private EditText textField;
     private Button connectPhones;
     private Button Stopthread;
-	private String messsage;
+    private String messsage;
     private EditText serverIp;
- //   private ListView LStrIn;
     private String serverIpAddress = "";
     private BroadcastReceiver receiver;
     private boolean connected = false;
     Intent intent;
     Thread cThread;
-    static int Mcounter =0;
-  //  private Handler handler = new Handler();
-  //  final Context context = this;
- //   String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
-
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_slimple_text_client);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slimple_text_client);
 
-         cThread  = new Thread(new ClientThread());
+        cThread = new Thread(new ClientThread());
 
         serverIp = (EditText) findViewById(R.id.editText1); // reference to the text field
-		connectPhones = (Button) findViewById(R.id.button1); // reference to the send button
+        connectPhones = (Button) findViewById(R.id.button1); // reference to the send button
         Stopthread = (Button) findViewById(R.id.button2); // reference to the send button
-  //      LStrIn = (ListView) findViewById(R.id.LStrIn); // reference to the send button
-
-     //   ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_slimple_text_client, mobileArray);
-      //  LStrIn .setAdapter(adapter);
-
-        // your oncreate code
-
         intent = new Intent();
 
         intent.setAction("com.tutorialspoint.CUSTOM_INTENT");
@@ -80,28 +92,22 @@ public class SlimpleTextClientActivity extends Activity {
         BR_DataRecieveHandle = new DataRecieveHandle();
         BR_DataRecieveHandle.setMainActivityHandler(this);
         IntentFilter fltr_smsreceived = new IntentFilter("com.tutorialspoint.CUSTOM_INTENT");
-        registerReceiver(BR_DataRecieveHandle,fltr_smsreceived);
-
-
-
+        registerReceiver(BR_DataRecieveHandle, fltr_smsreceived);
         // Button press event listener
         connectPhones.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View v) {
-
-                Log.d("SlimpleTextClientActivity", "ddddddd");
-               if (!connected) {
-                   Log.d("SlimpleTextClientActivity", "ddddddd2");
-
-                   serverIpAddress = serverIp.getText().toString();
-                   serverIpAddress= "192.168.1.6";
-                   Log.d("SlimpleTextClientActivity","serverIp= "+ serverIpAddress);
-                    if (!serverIpAddress.equals("")) {
-                        cThread.start();
-                    }
-                }
-			}
-		}
+                                             public void onClick(View v) {
+                                                 Log.d("SlimpleTextClientActivity", "ddddddd");
+                                                 if (!connected) {
+                                                     Log.d("SlimpleTextClientActivity", "ddddddd2");
+                                                     serverIpAddress = serverIp.getText().toString();
+                                                     serverIpAddress = "192.168.1.6";
+                                                     Log.d("SlimpleTextClientActivity", "serverIp= " + serverIpAddress);
+                                                     if (!serverIpAddress.equals("")) {
+                                                         cThread.start();
+                                                     }
+                                                 }
+                                             }
+                                         }
         );
       /*  Stopthread.setOnClickListener(new View.OnClickListener() {
 
@@ -109,54 +115,49 @@ public class SlimpleTextClientActivity extends Activity {
                 if (cThread != null) cThread.interrupt();
 
             }});*/
-	}
+    }
+
     public class ClientThread implements Runnable {
-
         public void run() {
-
             try {
-                byte [] mybytearray  = new byte [2000];
+                byte[] mybytearray = new byte[2000];
                 InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
                 Log.d("SlimpleTextClientActivity", "inthread");
                 Socket socket = new Socket("192.168.1.6", 502);
-             //   DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-          //      BufferedReader inputSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //   DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                //      BufferedReader inputSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 Log.d("SlimpleTextClientActivity", "C: Sending command.");
                 PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
                 // where you issue the commands
-
                 connected = true;
-                while (connected &&  Mcounter++<1000) {
+                while (connected) {
                     try {
-                //        out.println("Hey Server!");
-
                         int LengthRecieve = socket.getInputStream().available();
-                        Log.d("SlimpleTextClientActivity", "WAIT ssst Length := " + LengthRecieve );
-                        String response = "@";
-
-                      //  while (socket.getInputStream().available() > 0) {
+                        if (LengthRecieve > 0) {
+                            Log.d("SlimpleTextClientActivity", "WAIT ssst Length := " + LengthRecieve);
+                            String response = "@";
                             int ready = socket.getInputStream().available();
                             byte[] bytes = new byte[ready];
-                            response = response+socket.getInputStream().read(bytes,0,ready);
-
-                            intent.putExtra("Data",bytes);
-                            intent.putExtra("Data_Length",ready);
-                        sendBroadcast(intent);
-                      //  }
-                        for(int i = 0 ;i < ready ; i++)
-                            response = response +","+ (char)bytes[i];
+                            response = response + socket.getInputStream().read(bytes, 0, ready);
+                            intent.putExtra("Data", bytes);
+                            intent.putExtra("Data_Length", ready);
+                            sendBroadcast(intent);
+                            //  }
+                      /*  for(int i = 0 ;i < ready ; i++)
+                            response = response +","+ bytes[i];
                         response = response+"#";
-                        Log.d("SlimpleTextClientActivity", "response = " + response);
+                        Log.d("SlimpleTextClientActivity", "response = " + response);*/
                     /*    String  incomingMessage = inputSocket.readLine();
                         if (incomingMessage != null ) {
-
                             Log.d("SlimpleTextClientActivity", "Recieve ssst"+incomingMessage  );}
-                   */     Log.d("SlimpleTextClientActivity", "Recieve ssst2" );
-                        Thread.sleep(50);
-                    //    if (response != null)
-                          //  Log.d("SlimpleTextClientActivity", "Recieve from client" + response);
-                      } catch (Exception e) {
+                   */
+                            Log.d("SlimpleTextClientActivity", "Recieve ssst2");
+                            Thread.sleep(50);
+                            //    if (response != null)
+                            //  Log.d("SlimpleTextClientActivity", "Recieve from client" + response);
+                        }
+                    } catch (Exception e) {
                         Log.e("SlimpleTextClientActivity", "S: Error", e);
                     }
                 }
@@ -168,57 +169,45 @@ public class SlimpleTextClientActivity extends Activity {
             }
         }
     }
-
-	private class SendMessage extends AsyncTask<Void, Void, Void> {
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			try {
-
-				client = new Socket("192.168.1.6", 502); // connect to the server
-
-				printwriter = new PrintWriter(client.getOutputStream(), true);
-				printwriter.write(messsage); // write the message to output stream
-
-				printwriter.flush();
-				printwriter.close();
-				client.close(); // closing the connection
-
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.slimple_text_client, menu);
-		return true;
-	}
+    private class SendMessage extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                client = new Socket("192.168.1.6", 502); // connect to the server
+                printwriter = new PrintWriter(client.getOutputStream(), true);
+                printwriter.write(messsage); // write the message to output stream
+                printwriter.flush();
+                printwriter.close();
+                client.close(); // closing the connection
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.slimple_text_client, menu);
+        return true;
+    }
     // broadcast a custom intent.
-    public void broadcastIntent(View view){
-
-
-
-             byte[] bytes = new byte[100];
-        for(int i=0 ;i<20;i++) bytes[i] = (byte) i;
-        intent.putExtra("Data",bytes);
+    public void broadcastIntent(View view) {
+        byte[] bytes = new byte[100];
+        for (int i = 0; i < 20; i++) bytes[i] = (byte) i;
+        intent.putExtra("Data", bytes);
         sendBroadcast(intent);
     }
-    public void setViewNoStatic(String StrValue){
-        Log.d("SlimpleTextClientActivity", "set view ="+ StrValue);
-        serverIp.setText(StrValue);
+    public void setViewNoStatic( JSONObject jsonRootObject) {
+//        Log.d("SlimpleTextClientActivity", "set view =" + StrValue);
+
+        serverIp.setText(jsonRootObject.toString());
+
     }
-
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
     }
 }
